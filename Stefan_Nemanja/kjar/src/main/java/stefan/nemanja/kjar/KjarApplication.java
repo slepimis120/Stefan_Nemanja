1100b27edd4462588aa75e085d1bd77402480426
slepimis120 <slepimis120@gmail.com>
2024-04-22 01:24:11 +0200

    feat: Add Project Proposal and create project structure

---
+++ Stefan_Nemanja/kjar/src/main/java/stefan/nemanja/kjar/KjarApplication.java
@@ -0,0 +1,13 @@
+package stefan.nemanja.kjar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KjarApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello from kjar!");
+	}
+
+}
